7eb0f507c4cab2ba4097f141b5cacbe00d54dcd9
Ayush1010101 <ayushero100@gmail.com>
2026-04-15 17:38:00 +0530

    Landing Page + dashboard Modification

--- review-bridge-frontend/src/App.jsx
+++ review-bridge-frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
 import ReviewBridgeHome from './ReviewBridgeHome';
 
 function App() {
   return (
-    <ReviewBridgeHome />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<ReviewBridgeHome />} />
+    </Routes>
   );
 }
 
